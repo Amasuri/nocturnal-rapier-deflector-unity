75c2703f68544e8aee9d6cb277a406bede198888
keiondesu96 <keiondesu96@gmail.com>
2022-04-07 23:08:40 +0400

    timer (not functional yet)

--- Assets/scripts/Logic/BattleControl.cs
+++ Assets/scripts/Logic/BattleControl.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -5,6 +6,8 @@ using UnityEngine;
 
 public class BattleControl : MonoBehaviour
 {
+    private const float TimeLimitSec = 90;
+    private const float TimeLimitBossSec = 120;
     public float TimeLeftSec { get; private set; }
 
     public TextMeshProUGUI descTextTerminal;
@@ -13,11 +16,25 @@ public class BattleControl : MonoBehaviour
     private void Start()
     {
         descTextTerminal = gameObject.GetComponent<TextMeshProUGUI>();
+        ResetTimer();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        descTextTerminal.text = string.Format("Witch: {0}      You: {1}", ScoreCounter.WitchHits, ScoreCounter.RapierHits);
+        TimeLeftSec -= Time.deltaTime;
+        if(TimeLeftSec <= 0f)
+        {
+            //todo
+        }
+
+        string minutes = ((int)TimeLeftSec / 60).ToString();
+        string seconds = ((int)TimeLeftSec % 60).ToString();
+        descTextTerminal.text = string.Format("Witch: {0}    {2}:{3}    You: {1}", ScoreCounter.WitchHits, ScoreCounter.RapierHits, minutes, seconds);
+    }
+
+    public void ResetTimer(bool isBoss = false)
+    {
+        TimeLeftSec = isBoss ? TimeLimitBossSec : TimeLimitSec;
     }
 }
