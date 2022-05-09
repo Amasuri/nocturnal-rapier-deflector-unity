db0a84b67107ae11c4ebc9fbd54815b53f238e00
keiondesu96 <keiondesu96@gmail.com>
2022-05-09 04:53:36 +0400

    full parallax asset + code

--- Assets/scripts/BackgroundScroll.cs
+++ Assets/scripts/BackgroundScroll.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class BackgroundScroll : MonoBehaviour
 {
     public RelativePosition relativePosition = RelativePosition.Center;
+    public RelativeDepth relativeDepth = RelativeDepth.Base;
     public enum RelativePosition
     {
         Center,
@@ -13,7 +14,18 @@ public class BackgroundScroll : MonoBehaviour
         Right
     }
 
-    public float ScrollSpeed = 0.15f;
+    public enum RelativeDepth
+    {
+        Base,
+        Parallax1,
+        Parallax2
+    }
+
+    private readonly float ScrollSpeedBase = 0.25f;
+    private readonly float ScrollSpeedParallax1 = 0.15f;
+    private readonly float ScrollSpeedParallax2 = 0.05f;
+
+    public SpriteRenderer rend;
 
     private float lBound;
     private float rBound;
@@ -21,31 +33,50 @@ public class BackgroundScroll : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        var img = gameObject.GetComponent<SpriteRenderer>();
+        rend = gameObject.GetComponent<SpriteRenderer>();
 
         //Get different image if loading on different battle scene
-        if(BattleControl.CurrentBattleType == BattleControl.BattleType.Second)
-            img.sprite = Resources.Load<Sprite>("sprite/bg/background1");
-        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.ThirdBoss)
-            img.sprite = Resources.Load<Sprite>("sprite/bg/background2");
+        //Scene 1 Parallax 1-3
+        if (BattleControl.CurrentBattleType == BattleControl.BattleType.First && relativeDepth == RelativeDepth.Base)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background0");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.First && relativeDepth == RelativeDepth.Parallax1)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background0_1");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.First && relativeDepth == RelativeDepth.Parallax2)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background0_2");
+
+        //Scene 2 Parallax 1-3
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.Second && relativeDepth == RelativeDepth.Base)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background1");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.Second && relativeDepth == RelativeDepth.Parallax1)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background1_1");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.Second && relativeDepth == RelativeDepth.Parallax2)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background1_2");
+
+        //Scene 3 Parallax 1-3
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.ThirdBoss && relativeDepth == RelativeDepth.Base)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background2");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.ThirdBoss && relativeDepth == RelativeDepth.Parallax1)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background2_1");
+        else if (BattleControl.CurrentBattleType == BattleControl.BattleType.ThirdBoss && relativeDepth == RelativeDepth.Parallax2)
+            rend.sprite = Resources.Load<Sprite>("sprite/bg/background2_2");
 
         lBound = 0f;
-        rBound = img.bounds.size.x;
+        rBound = rend.bounds.size.x;
 
         if (relativePosition == RelativePosition.Center)
         {
             lBound = 0f;
-            rBound = img.bounds.size.x;
+            rBound = rend.bounds.size.x;
         }
         else if (relativePosition == RelativePosition.Left)
         {
-            lBound = -img.bounds.size.x;
+            lBound = -rend.bounds.size.x;
             rBound = 0f;
         }
         else if (relativePosition == RelativePosition.Right)
         {
-            lBound = img.bounds.size.x;
-            rBound = img.bounds.size.x * 2;
+            lBound = rend.bounds.size.x;
+            rBound = rend.bounds.size.x * 2;
         }
 
         transform.position = new Vector2(lBound, transform.position.y);
@@ -54,8 +85,14 @@ public class BackgroundScroll : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        var scrollspd = ScrollSpeedBase;
+        if (relativeDepth == RelativeDepth.Parallax1)
+            scrollspd = ScrollSpeedParallax1;
+        else if (relativeDepth == RelativeDepth.Parallax2)
+            scrollspd = ScrollSpeedParallax2;
+
         //Moving base position
-        var movement = new Vector2(ScrollSpeed, 0) * Time.deltaTime;
+        var movement = new Vector2(scrollspd, 0) * Time.deltaTime;
         transform.Translate(movement);
 
         if (transform.position.x > rBound)
