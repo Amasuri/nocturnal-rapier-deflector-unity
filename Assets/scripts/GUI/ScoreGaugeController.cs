f35e8cdb00a86e56a298cca4881ccb74deeca92b
keiondesu96 <keiondesu96@gmail.com>
2022-05-10 23:39:36 +0400

    gauge sort of working... but very WIP

--- Assets/scripts/GUI/ScoreGaugeController.cs
+++ Assets/scripts/GUI/ScoreGaugeController.cs
@@ -4,32 +4,49 @@ using UnityEngine;
 
 public class ScoreGaugeController : MonoBehaviour
 {
-    private const float posDefault = 0.6155f;
+    private const float posDefaultWin = 0.6155f;
     private const float posMiddle = 0.5f;
     private const float posMaxLeft = 0f;
     private const float posMaxRight = 1f;
 
+    private float xPos;
+
+    private SpriteRenderer img;
+    private Vector2 origSize;
+
     // Start is called before the first frame update
     private void Start()
     {
+        img = GetComponent<SpriteRenderer>();
+        origSize = img.size;
     }
 
     // Update is called once per frame
     private void Update()
     {
+        ChangeGaugePosByRatio();
+
+        img.size = new Vector2(origSize.x * (posMaxRight - xPos), origSize.y);
+
+        //tex.Resize(origSizeX, tex.height); //(int)(origSizeX * (1f - xPos))
+    }
+
+    private void ChangeGaugePosByRatio()
+    {
         var ratio = ScoreCounter.GetScoreRatio();
         var obj = GetComponent<Transform>();
-        var xPos = obj.position.x;
+        xPos = obj.position.x;
 
-        xPos = posDefault * ratio;
+        xPos = posDefaultWin * ratio;
+
+        //Making the gauge appear less dramatic
+        if (xPos > posDefaultWin)
+            xPos = posDefaultWin + xPos / 10;
+
+        //if (xPos < posDefaultWin)
+        //    xPos = posDefaultWin - xPos / 10;
+
         xPos = Mathf.Clamp(xPos, posMaxLeft, posMaxRight);
-
-        //Mathf.Lerp
-
-        //if (xPos > posMaxRight)
-        //    xPos = posMaxRight;
-        //if (xPos < posMaxLeft)
-        //    xPos = posMaxLeft;
 
         obj.position = new Vector3(xPos, obj.position.y, obj.position.z);
     }
