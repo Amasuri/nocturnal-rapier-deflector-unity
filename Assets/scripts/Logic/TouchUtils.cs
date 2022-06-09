2c427188630276b857e973151edea5b837f584c5
keiondesu96 <keiondesu96@gmail.com>
2022-06-09 06:34:12 +0400

    minor mobile fixes

--- Assets/scripts/Logic/TouchUtils.cs
+++ Assets/scripts/Logic/TouchUtils.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class TouchUtils : MonoBehaviour
 {
     /// <summary>
-    /// Returns true only on handhelds. Checks if double-tap happened.
+    /// Returns true only on handhelds (& possibly Mac OS). Checks if double-tap happened.
     /// </summary>
     static public bool IsDoubleTap()
     {
-        bool isMobile = SystemInfo.deviceType == DeviceType.Handheld;
+        //Originally intended to be handhelds only, but i'm thinking of Mac OS port too. Need to debug that later
+        //to see if Mac OS supports the same API
+        bool isMobile = true;
         bool isDoubleTap = false;
         if (isMobile && Input.GetTouch(0).tapCount == 2)
             isDoubleTap = true;
