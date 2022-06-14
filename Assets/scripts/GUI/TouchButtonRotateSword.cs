e8883feccfed47fb62a35ac5d51c96daa92361fe
keiondesu96 <keiondesu96@gmail.com>
2022-06-14 12:04:14 +0400

    todo update + couldnt fix bug but future method for it is here

--- Assets/scripts/GUI/TouchButtonRotateSword.cs
+++ Assets/scripts/GUI/TouchButtonRotateSword.cs
@@ -37,14 +37,38 @@ public class TouchButtonRotateSword : MonoBehaviour
 
     private void OnMouseOver()
     {
+        //Currently only can process first touch on device
         if (!pressable)
             return;
 
         if (Input.GetMouseButtonDown(0))
         {
-            currentTouchDelay = maxTouchDelay;
-            rend.sprite = pressed;
-            Rapier.refToCurrentRapier.RotateRapier();
+            PressRapierButton();
         }
+    }
+
+    /// <summary>
+    /// Currently isn't used, because I didn't yet had the chance to debug multi-touch. Should be placed in Update()
+    /// </summary>
+    private void CheckMultitouchAnyFingerButtonPress()
+    {
+        if (!pressable)
+            return;
+
+        var buttonCollider = gameObject.GetComponent<Collider2D>();
+        foreach (var touch in Input.touches)
+        {
+            var touchInBox = buttonCollider.bounds.Contains(Camera.main.ScreenToWorldPoint(touch.position));
+            var touchJustPressed = touch.phase == TouchPhase.Began;
+            if (touchInBox && touchJustPressed)
+                PressRapierButton();
+        }
+    }
+
+    private void PressRapierButton()
+    {
+        currentTouchDelay = maxTouchDelay;
+        rend.sprite = pressed;
+        Rapier.refToCurrentRapier.RotateRapier();
     }
 }
